d211bf9a77118b44542df69385dbc13f2dbe4d85
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-06-06 20:17:57 +0100

    server details

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require ('express');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') require ('dotenv').config();
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(cors());
+app.use(cors());
 
 app.use('/mailMe', require('./mailMe'))
 
