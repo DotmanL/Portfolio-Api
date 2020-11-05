031ea00bfcc1c6c33891461d714b792aa090cd30
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-11-05 18:11:08 +0100

    port changes

--- mailMe.js
+++ mailMe.js
@@ -2,59 +2,55 @@ const express = require('express')
 const mailMeRouter = express.Router()
 const nodemailer = require('nodemailer')
 
-
 const transport = {
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.THE_EMAIL,
-    pass: process.env.thePassword,
-  },
-  // debug: true,
-  // logger: true
-};
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: process.env.THE_EMAIL,
+		pass: process.env.thePassword,
+	},
+	// debug: true,
+	// logger: true,
+}
 
-  const transporter = nodemailer.createTransport(transport);
-  transporter.verify((error, success) => {
-    if(error) {
-      console.error(error)
-    } else {
-      console.log('users ready to mail myself')
-    }
-  });
+const transporter = nodemailer.createTransport(transport)
+transporter.verify((error, success) => {
+	if (error) {
+		console.error(error)
+	} else {
+		console.log('users ready to mail myself')
+	}
+})
 
+mailMeRouter.post('/', (req, res, next) => {
+	const mail = {
+		from: process.env.THE_EMAIL,
+		to: 'oladotunlawal7@gmail.com',
+		text: `
 
-  mailMeRouter.post('/', (req,res, next) => {
-    const mail = {
-      from: process.env.THE_EMAIL,
-      to: 'oladotunlawal7@gmail.com',
-      text: `
-
-      from: ${req.body.email} 
+      from: ${req.body.email}
 
       name: ${req.body.name}
 
       subject: ${req.body.subject}
 
-      message:  ${req.body.message}`
-    }
+      message:  ${req.body.message}`,
+	}
 
-transporter.sendMail(mail, (err,data) => {
-    if(err) {
-      res.json({
-        status: 'failure to send mail'
-      })
-    } else {
-      res.json({
-        status: 'success'
-      })
-    }
-  }) 
+	transporter.sendMail(mail, (err, data) => {
+		if (err) {
+			res.json({
+				status: 'failure to send mail',
+			})
+		} else {
+			res.json({
+				status: 'success',
+			})
+		}
+	})
+})
 
-});
-
-
-console.log("from sendToMe")
+console.log('from sendToMe')
 
 module.exports = mailMeRouter
